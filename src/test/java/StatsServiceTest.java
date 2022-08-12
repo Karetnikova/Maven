6372e7e9365eb395f4a86cb45b9ee239ff23dca2
Karetnikova <Karetnikovala@mail.ru>
2022-08-12 21:56:19 +0800

    Massiv

---
+++ src/test/java/StatsServiceTest.java
@@ -0,0 +1,49 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+public class StatsServiceTest {
+
+    @Test
+    public void AllSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void averageSalesMonth() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.average(sales);
+        long expected = (8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18) / 12;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void belowAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long actual = service.avgless();
+        long expected = ;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int actual = service.maxSales(sales);
+        int expected = 5 + 1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void minSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int actual = service.minSales(sales);
+        int expected = 8 + 1;
+        assertEquals(expected, actual);
+    }
+}
